32daf586d39926cd2ba5cf16e6e80e5104c8a3bf
AliMctavish <alifadil4141@gmail.com>
2023-06-16 03:38:43 +0300

    done with main menu

--- gravityProject/MainMenu.cs
+++ gravityProject/MainMenu.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace gravityProject
 {
-    public  class MainMenu
+    public class MainMenu
     {
         //DECLARING MOUSE POSITION
         private Rectangle mousePos;
@@ -20,6 +20,7 @@ namespace gravityProject
         private MainMenuButtons startButton = new MainMenuButtons();
         private MainMenuButtons aboutButton = new MainMenuButtons();
         private MainMenuButtons exitButton = new MainMenuButtons();
+        private MainMenuButtons backButton = new MainMenuButtons();
 
         //DECLARING HIGHT AND WIDTH FOR THE BUTTONS
         private int hight = 100;
@@ -29,7 +30,7 @@ namespace gravityProject
         //DECLARING GLOBALS
         private ContentManager Content;
         private SpriteBatch SpriteBatch;
-        
+
         public void init()
         {
             //INIT GLOBALS
@@ -48,42 +49,46 @@ namespace gravityProject
 
             exitButton.position = new Rectangle(1400 / 2, 1200 / 2, width, hight);
             exitButton.texture = Content.Load<Texture2D>("exit-export");
+
+            backButton.position = new Rectangle(1000 / 2, 1200 / 2, width, hight);
+            backButton.texture = Content.Load<Texture2D>("backButton-export");
         }
         public void ButtonSelect()
         {
             this.UseBackGround();
 
-            if (Game1.gameInfo == true)
-                this.GameInfoMenu();
-
             this.init();
 
-            if (mousePos.Intersects(startButton.position))
+            if (mousePos.Intersects(startButton.position) && !Game1.gameInfo)
             {
                 SpriteBatch.Draw(startButton.texture, startButton.position, Color.Yellow);
                 if (Mouse.GetState().LeftButton == ButtonState.Pressed)
                     Game1.gameStarted = true;
             }
             else
-                SpriteBatch.Draw(startButton.texture,startButton.position, Color.White);
+                SpriteBatch.Draw(startButton.texture, startButton.position, Color.White);
 
-            if(mousePos.Intersects(aboutButton.position))
+            if (mousePos.Intersects(aboutButton.position))
             {
-                SpriteBatch.Draw(aboutButton.texture,aboutButton.position, Color.Yellow);
-                if(Mouse.GetState().LeftButton == ButtonState.Pressed)
+                SpriteBatch.Draw(aboutButton.texture, aboutButton.position, Color.Yellow);
+                if (Mouse.GetState().LeftButton == ButtonState.Pressed)
                     Game1.gameInfo = true;
             }
             else
-                SpriteBatch.Draw(aboutButton.texture,aboutButton.position, Color.White);
+                SpriteBatch.Draw(aboutButton.texture, aboutButton.position, Color.White);
 
-            if(mousePos.Intersects(exitButton.position))
+            if (mousePos.Intersects(exitButton.position) && !Game1.gameInfo)
             {
-                SpriteBatch.Draw(exitButton.texture, exitButton.position,Color.Yellow);
-                if(Mouse.GetState().LeftButton == ButtonState.Pressed)
+                SpriteBatch.Draw(exitButton.texture, exitButton.position, Color.Yellow);
+                if (Mouse.GetState().LeftButton == ButtonState.Pressed)
                     Game1.exitGame = true;
             }
             else
-                SpriteBatch.Draw(exitButton.texture, exitButton.position,Color.White);
+                SpriteBatch.Draw(exitButton.texture, exitButton.position, Color.White);
+
+
+            if (Game1.gameInfo == true)
+                this.GameInfoMenu();
 
 
             //MOUSE CURSOR
@@ -97,9 +102,17 @@ namespace gravityProject
 
         public void GameInfoMenu()
         {
-            Globals.spriteBatch.Draw(Globals.Content.Load<Texture2D>("background-export"), new Rectangle(0, 0, 1600, 900), Color.White);
+            SpriteBatch.Draw(Content.Load<Texture2D>("backJanaAbout"), new Rectangle(0, 0, 1600, 900), Color.White);
 
+            if (mousePos.Intersects(backButton.position))
+            {
+                SpriteBatch.Draw(backButton.texture, backButton.position, Color.Yellow);
+
+                if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+                    Game1.gameInfo = false;
+            }
+            else
+                SpriteBatch.Draw(backButton.texture, backButton.position, Color.White);
         }
-
     }
 }
